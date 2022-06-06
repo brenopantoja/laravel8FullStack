34900cba18fd80ce485aa7caebb16f11a5a4c269
Breno Pantoja <breno.r.pantoja@gmail.com>
2022-06-06 11:35:48 -0300

    06062022

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -13,7 +13,9 @@
     <div class="row">
 
 
-        <table class="table">
+        ,<!--<table class="table"> -->
+        <table class="table table-striped table-bordered table-condensed table-hover">
+
             <thead>
                 <tr>
                     <th scop="col">#</th>
